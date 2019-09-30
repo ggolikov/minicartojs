b688d57b7972e1950d48e7c3bbcfcef5f9b6d1cf
Grigory Golikov <gr.golikov@gmail.com>
2019-09-30 08:34:14 -0700

    Request API over https

--- src/constants.js
+++ src/constants.js
@@ -1,3 +1,3 @@
-const MAPS_API_URL = 'http://documentation.cartodb.com/api/v1/map';
+const MAPS_API_URL = 'https://documentation.cartodb.com/api/v1/map';
 const TEMPLATE_URL = 'https://cartocdn-gusc-d.global.ssl.fastly.net';
 export { MAPS_API_URL, TEMPLATE_URL }
