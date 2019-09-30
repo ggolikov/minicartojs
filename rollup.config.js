61fc9dd9232f33e3852392f1ec4a547864a9eec1
Grigory Golikov <gr.golikov@gmail.com>
2019-09-30 07:19:28 -0700

    Update rollup config

--- rollup.config.js
+++ rollup.config.js
@@ -7,16 +7,16 @@ export default {
 	input: 'src/index.js',
 	output: {
 		file: 'dist/index.js',
-		format: 'esm', // immediately-invoked function expression — suitable for <script> tags
-		// format: 'iife', // immediately-invoked function expression — suitable for <script> tags
+		format: 'umd',
+		name: 'miniCarto',
 		sourcemap: false
 	},
 	plugins: [
 		babel({
 			exclude: 'node_modules/**',
 		}),
-		resolve(), // tells Rollup how to find date-fns in node_modules
-		commonjs(), // converts date-fns to ES modules
-		terser() // minify, but only in production
+		resolve(),
+		commonjs(),
+		terser()
 	]
 };
